92ac96149c186b86dfc3bdc06215aac6f4f7e9ef
José Antonio <neoxisti@gmail.com>
2020-04-13 15:27:56 +0200

    CSV zip working

--- modules/stability.R
+++ modules/stability.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(shinyjs)
-library(R.utils)
 
 # Stability UI ----
 stabilityUI <- function(id) {
@@ -11,31 +10,31 @@ stabilityUI <- function(id) {
     #tags$hr(),
     fluidRow(
       column(12, wellPanel(id = "stabilityConf", 
-                     style = "overflow: hidden;", 
-                     fluidRow( # Row of configs
-                      column(12, tags$h5("Configuration parameters")),
-                      column(3, numericInput(ns("kmin"), "Min. num. of clusters:", 2, min = 2, max = 15)),
-                      column(3, numericInput(ns("kmax"), "Max. num. of clusters:", 3, min = 2, max = 15)),
-                      column(3, numericInput(ns("bs"), "Bootstrap:", 20, min = 20, max = 500, step=10)),
-                      column(3, numericInput(ns("seed"), "Seed:", 20, min = 1, step=10))
-                     ),
-                     fluidRow( # Row of buttons
-                        column(3, actionButton(ns("btnExecute"), "Execute", icon("terminal"), 
-                               style="color: #fff; background-color: #337ab7; ")),
-                        column(3, hidden(downloadButton(ns("btnDownloadCSV"), "CSV", 
-                               style="color: #fff; background-color: #0c9463; "))),
-                        column(3, hidden(downloadButton(ns("btnDownloadImages"), "Images", 
-                               style="color: #fff; background-color: #0c9463; ")))
-                     )
-                ) # End well panel
-             )
+                           style = "overflow: hidden;", 
+                           fluidRow( # Row of configs
+                             column(12, tags$h5("Configuration parameters")),
+                             column(3, numericInput(ns("kmin"), "Min. num. of clusters:", 2, min = 2, max = 15)),
+                             column(3, numericInput(ns("kmax"), "Max. num. of clusters:", 3, min = 2, max = 15)),
+                             column(3, numericInput(ns("bs"), "Bootstrap:", 20, min = 20, max = 500, step=10)),
+                             column(3, numericInput(ns("seed"), "Seed:", 20, min = 1, step=10))
+                           ),
+                           fluidRow( # Row of buttons
+                             column(3, actionButton(ns("btnExecute"), "Execute", icon("terminal"), 
+                                                    style="color: #fff; background-color: #337ab7; ")),
+                             column(3, hidden(downloadButton(ns("btnDownloadCSV"), "CSV", 
+                                                             style="color: #fff; background-color: #0c9463; "))),
+                             column(3, hidden(downloadButton(ns("btnDownloadImages"), "Images", 
+                                                             style="color: #fff; background-color: #0c9463; ")))
+                           )
+      ) # End well panel
+      )
       #, column(12, tags$hr())
     ),
     fluidRow(
       column(12, 
              div(style="max-height: 200px; width: 100%; border-left: 5px solid #2196F3; background-color: #ddffff; padding-left: 10px; overflow-y: auto;",
                  textOutput(ns("evaluomeROutput")))
-            )
+      )
     ),
     tags$hr(),
     # Stability output div handled by 'stability' function ----
@@ -50,9 +49,9 @@ stabilityUI <- function(id) {
 stability <- function(input, output, session, data) {
   
   results <- reactiveValues(
-                stabilityData=NULL,
-                visibleDownloadButtons=FALSE
-             )
+    stabilityData=NULL,
+    visibleDownloadButtons=FALSE
+  )
   
   output$stabilityResult <- renderUI({
     if (is.null(data$inputData) || data$inputData == "" || is.null(data$inputDf)) {
@@ -60,25 +59,34 @@ stability <- function(input, output, session, data) {
     }
   })
   
+  # If input data changes, reset and disable buttons
+  observeEvent(data$inputData, {
+    results$stabilityData=NULL
+    results$visibleDownloadButtons = FALSE
+  })
+  
   output$btnDownloadCSV <- downloadHandler(
-    filename = function() {
-      paste("stabilityData", ".tar.gz", sep = "")
+    filename = function(){
+      paste0("stabilityData",".zip")
+      
     },
-    content = function(file) {
-      #write.csv("data", file)
-      tmpdir <- tempdir()
-      setwd(tempdir())
-      csvNameList = unlist(lapply(names(results$stabilityData), 
-                           function(csvName) {paste0(csvName, ".csv")}), use.names=FALSE)
-      print(list.files(path = "."))
-      dfName = names(results$stabilityData)
-      for (i in 1:length(csvNameList)) {
-        #print(assay(results$stabilityData[dfName[i]]))
-        write.csv(assay(results$stabilityData[dfName[i]]), file=csvNameList[i], row.names = FALSE)
+    content = function(file){
+      #go to a temp dir to avoid permission issues
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      files <- NULL;
+      
+      for (table_name in names(results$stabilityData)) {
+        fileName <- paste(table_name,".csv",sep = "")
+        write.table(assay(results$stabilityData[[table_name]]),
+                    fileName,
+                    sep = ',',
+                    row.names = F,
+                    col.names = T)
+        files <- c(fileName,files)
       }
-      # TODO: El tar está vacío
-      print(csvNameList)
-      tar(tarfile = file, files="stability_mean.csv")# compression="gzip")
+      #create the zip file
+      zip(file,files)
     }
   )
   
@@ -113,33 +121,35 @@ stability <- function(input, output, session, data) {
       shinyalert("Oops!", MSG_STABILITY_WENT_WRONG, type = "error")
       return(NULL)
     }
-
+    
+    # Render tables
     renderTablesTabs(output, results)
+    
     results$visibleDownloadButtons = TRUE
   })
-  #return(NULL)
+  return(NULL)
   
   #return (reactive({cat("--> ", data$inputData, "\n")}))
 }
 
 # Stability private functions ----
-# Runs an evaliomeR stability execution
+# Runs an evaluomeR stability execution
 runStability <- function(df, kmin, kmax, bs, seed) {
   cat(file=stderr(), 
       "Running stability index with kmin=",kmin,", kmax=",kmax,
       ", bs=", bs, ", seed=", seed,"\n")
-  shinyjs::disable("btnExecute")
+  
   result <- NULL
   withCallingHandlers({
     shinyjs::html("evaluomeROutput", "")
     result <- stabilityRange(data=df, k.range=c(kmin, kmax), 
-                   bs=bs, seed=seed, getImages = FALSE)
+                             bs=bs, seed=seed, getImages = FALSE)
   },
   message = function(m) {
-     shinyjs::html(id = "evaluomeROutput", html = paste0(m$message, "<br>"), add = TRUE)
-    })
-  shinyjs::enable("btnExecute")
-
+    shinyjs::html(id = "evaluomeROutput", html = paste0(m$message, "<br>"), add = TRUE)
+  })
+  #shinyjs::disable("btnExecute")
+  
   return(result)
 }
 
@@ -148,26 +158,24 @@ renderTablesTabs <- function(output, results) {
   output$stabilityResult <- renderUI({
     tabNum <- length(names(results$stabilityData))
     tabNames <- names(results$stabilityData)
-    csvNameList = lapply(names(results$stabilityData), 
-                         function(csvName) {paste0(csvName, ".csv")})
     
     tagList(
       tags$h5("Result tables"),
       fluidRow(
-            column(12,
-              do.call(tabsetPanel,
-                c(id='stabilityTables',
-                 lapply(1:tabNum, function(i) {
-                    tabName <- tabNames[i]
-                    tabPanel(
-                      title=paste0(tabName),
-                      renderTable({assay(results$stabilityData[tabName])})
-                    )
-                  }
-                )
-              )
-            )
-          )
+        column(12,
+               do.call(tabsetPanel,
+                       c(id='stabilityTables',
+                         lapply(1:tabNum, function(i) {
+                           tabName <- tabNames[i]
+                           tabPanel(
+                             title=paste0(tabName), 
+                             renderTable({assay(results$stabilityData[tabName])})
+                           )
+                         }
+                         )
+                       )
+               )
+        )
       ),
       tags$hr()
     )
